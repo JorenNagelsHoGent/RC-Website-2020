2f0a0c1d42cc75eb1fd556ffbf97113f4dbf7668
Joren Nagels <joren.nagels@student.hogent.be>
2020-12-12 17:45:51 +0100

    teams

--- teams/lol-teams.php
+++ teams/lol-teams.php
@@ -18,71 +18,71 @@ session_start();
 // When Discord redirects the user back here, there will be a "code" and "state" parameter in the query string
 if (get('code')) {
 
-	// Exchange the auth code for a token
-	$token = apiRequest($tokenURL, array(
-		"grant_type" => "authorization_code",
-		'client_id' => OAUTH2_CLIENT_ID,
-		'client_secret' => OAUTH2_CLIENT_SECRET,
-		'redirect_uri' => 'http://localhost:3000/index.php',
-		'code' => get('code')
-	));
-	$logout_token = $token->access_token;
-	$_SESSION['access_token'] = $token->access_token;
+    // Exchange the auth code for a token
+    $token = apiRequest($tokenURL, array(
+        "grant_type" => "authorization_code",
+        'client_id' => OAUTH2_CLIENT_ID,
+        'client_secret' => OAUTH2_CLIENT_SECRET,
+        'redirect_uri' => 'http://localhost:3000/index.php',
+        'code' => get('code')
+    ));
+    $logout_token = $token->access_token;
+    $_SESSION['access_token'] = $token->access_token;
 
 
-	header('Location: ' . $_SERVER['PHP_SELF']);
+    header('Location: ' . $_SERVER['PHP_SELF']);
 }
 
 if (session('access_token')) {
-	$user = apiRequest($apiURLBase, array());
-	$avatarURL = "https://cdn.discordapp.com/avatars/" . $user->id . "/" . $user->avatar . ".png";
+    $user = apiRequest($apiURLBase, array());
+    $avatarURL = "https://cdn.discordapp.com/avatars/" . $user->id . "/" . $user->avatar . ".png";
 }
 if (get('action') == 'login') {
-	header('Location: https://discord.com/api/oauth2/authorize?client_id=693302768340959333&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Findex.php&response_type=code&scope=identify%20email');
+    header('Location: https://discord.com/api/oauth2/authorize?client_id=693302768340959333&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Findex.php&response_type=code&scope=identify%20email');
 }
 
 if (get('action') == 'logout') {
-	// This must to logout you, but it didn't worked(
-	$params = array(
-		'access_token' => session('access_token')
-	);
+    // This must to logout you, but it didn't worked(
+    $params = array(
+        'access_token' => session('access_token')
+    );
 
-	// Redirect the user to Discord's revoke page
-	header('content_type:x-www-form-urlencoded','Location: https://discordapp.com/api/oauth2/token/revoke' . '?' . http_build_query($params));
-	$_SESSION = array();
+    // Redirect the user to Discord's revoke page
+    header('content_type:x-www-form-urlencoded', 'Location: https://discordapp.com/api/oauth2/token/revoke' . '?' . http_build_query($params));
+    $_SESSION = array();
 }
 
 function apiRequest($url, $post = FALSE, $headers = array())
 {
-	$ch = curl_init($url);
-	curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 
-	$response = curl_exec($ch);
+    $response = curl_exec($ch);
 
 
-	if ($post)
-		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+    if ($post)
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
 
-	$headers[] = 'Accept: application/json';
+    $headers[] = 'Accept: application/json';
 
-	if (session('access_token'))
-		$headers[] = 'Authorization: Bearer ' . session('access_token');
+    if (session('access_token'))
+        $headers[] = 'Authorization: Bearer ' . session('access_token');
 
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-	$response = curl_exec($ch);
-	return json_decode($response);
+    $response = curl_exec($ch);
+    return json_decode($response);
 }
 
 function get($key, $default = NULL)
 {
-	return array_key_exists($key, $_GET) ? $_GET[$key] : $default;
+    return array_key_exists($key, $_GET) ? $_GET[$key] : $default;
 }
 
 function session($key, $default = NULL)
 {
-	return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : $default;
+    return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : $default;
 }
 
 ?>
@@ -93,13 +93,11 @@ function session($key, $default = NULL)
 <head>
     <meta property="og:type" content="website">
     <meta property="og:title" content="Rocket Core" />
-    <meta property="og:description"
-        content="Rocket Core is a Rocket League Tournament and Events Organisation that fosters an enjoyable environment for all it's staff, members and everyone involved. Our aim is to run successful and enjoyable events for the community and have fun doing so!" />
+    <meta property="og:description" content="Rocket Core is a Rocket League Tournament and Events Organisation that fosters an enjoyable environment for all it's staff, members and everyone involved. Our aim is to run successful and enjoyable events for the community and have fun doing so!" />
     <meta property="og:url" content="https://rocketcorerl.com" />
     <meta property="og:image" content="https://www.rocketcorerl.com/img/logo_o.png" />
     <meta name="description" content="Rocket Core official website">
-    <meta name="keywords"
-        content="Rocket Core,Rocket,Core,Rocket League,League,tournament,tournaments,eu,esports,RC,RCEsports,rocket core esports,rcpluto,rcsaturn,rcjupiter,rcneptune,galactic,series">
+    <meta name="keywords" content="Rocket Core,Rocket,Core,Rocket League,League,tournament,tournaments,eu,esports,RC,RCEsports,rocket core esports,rcpluto,rcsaturn,rcjupiter,rcneptune,galactic,series">
     <meta name="author" content="Joren Nagels">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -107,7 +105,7 @@ function session($key, $default = NULL)
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
     <title>Rocket Core</title>
     <script type="text/javascript">
-        (function () {
+        (function() {
             var css = document.createElement('link');
             css.href = 'https://use.fontawesome.com/releases/v5.1.0/css/all.css';
             css.rel = 'stylesheet';
@@ -128,75 +126,25 @@ function session($key, $default = NULL)
     <?php require_once "../navbar.php" ?>
 
     <!-- Body -->
-    <div class="TeamBG" id="TeamBGESports">
-        <div class="main" id="TeamOverview">
-            <h1 class="Teamheader">ESports</h1>
-            <table class="TeamTable">
-                <tr>
-                    <td><a href="https://twitter.com/blitzleague_"><strong>Blitzleague</strong></a></td>
-                </tr>
-                <tr>
-                    <td>Jariz</td>
-                </tr>
-                <tr>
-                    <td>Kevster098</td>
-                </tr>
-            </table>
+    <article>
+        Follow our Official Rocket Core Esports Twitter <a href="https://twitter.com/RocketCore_E" target="_blank">@RocketCore_E <i class="fab fa-twitter"></i></a>
+    </article>
+    <!-- Body -->
+    <div class="teams">
+        <div class="collumn" id="esports">
+            <h2>Nova
+                <img src="../img/rook_emoji.png">
+            </h2>
+            <ul>
+                <li class="cap"><a href="https://twitter.com/TedGuru2" target="_blank">Ted Guru (Coach)<i class="fab fa-twitter"></i></a></li>
+                <li class="cap"><a href="https://twitter.com/RC_DragonRage" target="_blank">DragonRage <i class="fab fa-twitter"></i></a></li>
+                <li><a href="https://twitter.com/RC_Alurian" target="_blank">Alurian <i class="fab fa-twitter"></i></a></li>
+                <li>Keizer</li>
+                <li>Elijah</li>
+                <li><a href="https://twitter.com/LordKypower" target="_blank">LordKyPower <i class="fab fa-twitter"></i></a></li>
+            </ul>
         </div>
     </div>
-
-    <div class="TeamBG" id="TeamBGSaturn">
-        <div class="main">
-            <table class="TeamTable">
-                <tr>
-                    <td><a href="https://twitter.com/blitzleague_"><strong>Blitzleague</strong></a></td>
-                </tr>
-                <tr>
-                    <td>Jariz</td>
-                </tr>
-                <tr>
-                    <td>Kevster098</td>
-                </tr>
-            </table>
-            <h1 class="Teamheader">Saturn</h1>
-        </div>
-    </div>
-
-    <div class="TeamBG" id="TeamBGPluto">
-        <div class="main">
-            <h1 class="Teamheader">Pluto</h1>
-            <table class="TeamTable">
-                <tr>
-                    <td><a href="https://twitter.com/blitzleague_"><strong>Blitzleague</strong></a></td>
-                </tr>
-                <tr>
-                    <td>Jariz</td>
-                </tr>
-                <tr>
-                    <td>Kevster098</td>
-                </tr>
-            </table>
-        </div>
-    </div>
-
-    <div class="TeamBG" id="TeamBGSaturn">
-        <div class="main">
-            <table class="TeamTable">
-                <tr>
-                    <td><a href="https://twitter.com/blitzleague_"><strong>Blitzleague</strong></a></td>
-                </tr>
-                <tr>
-                    <td>Jariz</td>
-                </tr>
-                <tr>
-                    <td>Kevster098</td>
-                </tr>
-            </table>
-            <h1 class="Teamheader">Saturn</h1>
-        </div>
-    </div>
-
-
     <!--- footer -->
     <?php require_once "../footer.html" ?>
 
--- teams/rl-teams.php
+++ teams/rl-teams.php
@@ -18,71 +18,71 @@ session_start();
 // When Discord redirects the user back here, there will be a "code" and "state" parameter in the query string
 if (get('code')) {
 
-	// Exchange the auth code for a token
-	$token = apiRequest($tokenURL, array(
-		"grant_type" => "authorization_code",
-		'client_id' => OAUTH2_CLIENT_ID,
-		'client_secret' => OAUTH2_CLIENT_SECRET,
-		'redirect_uri' => 'http://localhost:3000/index.php',
-		'code' => get('code')
-	));
-	$logout_token = $token->access_token;
-	$_SESSION['access_token'] = $token->access_token;
+    // Exchange the auth code for a token
+    $token = apiRequest($tokenURL, array(
+        "grant_type" => "authorization_code",
+        'client_id' => OAUTH2_CLIENT_ID,
+        'client_secret' => OAUTH2_CLIENT_SECRET,
+        'redirect_uri' => 'http://localhost:3000/index.php',
+        'code' => get('code')
+    ));
+    $logout_token = $token->access_token;
+    $_SESSION['access_token'] = $token->access_token;
 
 
-	header('Location: ' . $_SERVER['PHP_SELF']);
+    header('Location: ' . $_SERVER['PHP_SELF']);
 }
 
 if (session('access_token')) {
-	$user = apiRequest($apiURLBase, array());
-	$avatarURL = "https://cdn.discordapp.com/avatars/" . $user->id . "/" . $user->avatar . ".png";
+    $user = apiRequest($apiURLBase, array());
+    $avatarURL = "https://cdn.discordapp.com/avatars/" . $user->id . "/" . $user->avatar . ".png";
 }
 if (get('action') == 'login') {
-	header('Location: https://discord.com/api/oauth2/authorize?client_id=693302768340959333&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Findex.php&response_type=code&scope=identify%20email');
+    header('Location: https://discord.com/api/oauth2/authorize?client_id=693302768340959333&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Findex.php&response_type=code&scope=identify%20email');
 }
 
 if (get('action') == 'logout') {
-	// This must to logout you, but it didn't worked(
-	$params = array(
-		'access_token' => session('access_token')
-	);
+    // This must to logout you, but it didn't worked(
+    $params = array(
+        'access_token' => session('access_token')
+    );
 
-	// Redirect the user to Discord's revoke page
-	header('content_type:x-www-form-urlencoded','Location: https://discordapp.com/api/oauth2/token/revoke' . '?' . http_build_query($params));
-	$_SESSION = array();
+    // Redirect the user to Discord's revoke page
+    header('content_type:x-www-form-urlencoded', 'Location: https://discordapp.com/api/oauth2/token/revoke' . '?' . http_build_query($params));
+    $_SESSION = array();
 }
 
 function apiRequest($url, $post = FALSE, $headers = array())
 {
-	$ch = curl_init($url);
-	curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 
-	$response = curl_exec($ch);
+    $response = curl_exec($ch);
 
 
-	if ($post)
-		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+    if ($post)
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
 
-	$headers[] = 'Accept: application/json';
+    $headers[] = 'Accept: application/json';
 
-	if (session('access_token'))
-		$headers[] = 'Authorization: Bearer ' . session('access_token');
+    if (session('access_token'))
+        $headers[] = 'Authorization: Bearer ' . session('access_token');
 
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-	$response = curl_exec($ch);
-	return json_decode($response);
+    $response = curl_exec($ch);
+    return json_decode($response);
 }
 
 function get($key, $default = NULL)
 {
-	return array_key_exists($key, $_GET) ? $_GET[$key] : $default;
+    return array_key_exists($key, $_GET) ? $_GET[$key] : $default;
 }
 
 function session($key, $default = NULL)
 {
-	return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : $default;
+    return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : $default;
 }
 
 ?>
@@ -93,13 +93,11 @@ function session($key, $default = NULL)
 <head>
     <meta property="og:type" content="website">
     <meta property="og:title" content="Rocket Core" />
-    <meta property="og:description"
-        content="Rocket Core is a Rocket League Tournament and Events Organisation that fosters an enjoyable environment for all it's staff, members and everyone involved. Our aim is to run successful and enjoyable events for the community and have fun doing so!" />
+    <meta property="og:description" content="Rocket Core is a Rocket League Tournament and Events Organisation that fosters an enjoyable environment for all it's staff, members and everyone involved. Our aim is to run successful and enjoyable events for the community and have fun doing so!" />
     <meta property="og:url" content="https://rocketcorerl.com" />
     <meta property="og:image" content="https://www.rocketcorerl.com/img/logo_o.png" />
     <meta name="description" content="Rocket Core official website">
-    <meta name="keywords"
-        content="Rocket Core,Rocket,Core,Rocket League,League,tournament,tournaments,eu,esports,RC,RCEsports,rocket core esports,rcpluto,rcsaturn,rcjupiter,rcneptune,galactic,series">
+    <meta name="keywords" content="Rocket Core,Rocket,Core,Rocket League,League,tournament,tournaments,eu,esports,RC,RCEsports,rocket core esports,rcpluto,rcsaturn,rcjupiter,rcneptune,galactic,series">
     <meta name="author" content="Joren Nagels">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -107,7 +105,7 @@ function session($key, $default = NULL)
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
     <title>Rocket Core</title>
     <script type="text/javascript">
-        (function () {
+        (function() {
             var css = document.createElement('link');
             css.href = 'https://use.fontawesome.com/releases/v5.1.0/css/all.css';
             css.rel = 'stylesheet';
@@ -117,7 +115,7 @@ function session($key, $default = NULL)
     </script>
     <link href="../css/style.css" rel="stylesheet">
     <link href="../css/teams.css" rel="stylesheet">
-    <script src="js/script.js"></script>
+    <script src="../js/script.js"></script>
 </head>
 
 <body>
@@ -127,72 +125,74 @@ function session($key, $default = NULL)
 
     <?php require_once "../navbar.php" ?>
 
+        <article>
+            Follow our Official Rocket Core Esports Twitter <a href="https://twitter.com/RocketCore_E" target="_blank">@RocketCore_E <i class="fab fa-twitter"></i></a>
+        </article>
     <!-- Body -->
-    <div class="TeamBG" id="TeamBGESports">
-        <div class="main" id="TeamOverview">
-            <h1 class="Teamheader">ESports</h1>
-            <table class="TeamTable">
-                <tr>
-                    <td><a href="https://twitter.com/blitzleague_"><strong>Blitzleague</strong></a></td>
-                </tr>
-                <tr>
-                    <td>Jariz</td>
-                </tr>
-                <tr>
-                    <td>Kevster098</td>
-                </tr>
-            </table>
+    <div class="teams">
+        <div class="collumn" id="esports">
+            <h2>Esports
+                <img src="../img/logo.png">
+            </h2>
+            <ul>
+                <li class="open">open</li>
+                <li class="open">open</li>
+                <li class="open">open</li>
+            </ul>
         </div>
-    </div>
-
-    <div class="TeamBG" id="TeamBGSaturn">
-        <div class="main">
-            <table class="TeamTable">
-                <tr>
-                    <td><a href="https://twitter.com/blitzleague_"><strong>Blitzleague</strong></a></td>
-                </tr>
-                <tr>
-                    <td>Jariz</td>
-                </tr>
-                <tr>
-                    <td>Kevster098</td>
-                </tr>
-            </table>
-            <h1 class="Teamheader">Saturn</h1>
+        <div class="collumn" id="topTeam">
+            <h2>Jupiter
+                <img src="../img/jupiter_emoji.png">
+            </h2>
+            <ul>
+                <li class="cap"><a href="https://twitter.com/its_Xynal" target="_blank">Xynal <i class="fab fa-twitter"></i></a></li>
+                <li class="open">open</li>
+                <li class="open">open</li>
+            </ul>
         </div>
-    </div>
-
-    <div class="TeamBG" id="TeamBGPluto">
-        <div class="main">
-            <h1 class="Teamheader">Pluto</h1>
-            <table class="TeamTable">
-                <tr>
-                    <td><a href="https://twitter.com/blitzleague_"><strong>Blitzleague</strong></a></td>
-                </tr>
-                <tr>
-                    <td>Jariz</td>
-                </tr>
-                <tr>
-                    <td>Kevster098</td>
-                </tr>
-            </table>
+        <div class="collumn" id="topTeam">
+            <h2>Venus
+                <img src="../img/venus_emoji.png">
+            </h2>
+            <ul>
+                <li class="cap"><a href="https://twitter.com/tvylics" target="_blank">Tvylics <i class="fab fa-twitter"></i></a></li>
+                <li class="open">open</li>
+                <li class="open">open</li>
+            </ul>
         </div>
-    </div>
-
-    <div class="TeamBG" id="TeamBGSaturn">
-        <div class="main">
-            <table class="TeamTable">
-                <tr>
-                    <td><a href="https://twitter.com/blitzleague_"><strong>Blitzleague</strong></a></td>
-                </tr>
-                <tr>
-                    <td>Jariz</td>
-                </tr>
-                <tr>
-                    <td>Kevster098</td>
-                </tr>
-            </table>
-            <h1 class="Teamheader">Saturn</h1>
+        <div class="collumn">
+            <h2>Saturn
+                <img src="../img/saturn_emoji.png">
+            </h2>
+            <ul>
+                <li class="cap"><a href="https://twitter.com/reen_rl" target="_blank">Reen <i class="fab fa-twitter"></i></a></li>
+                <li><a href="https://twitter.com/AnacaPlays" target="_blank">Anaca <i class="fab fa-twitter"></i></a></li>
+                <li><a href="https://twitter.com/sv_pigey" target="_blank">sv_pigey <i class="fab fa-twitter"></i></a></li>
+                <li class="sub"><a href="https://twitter.com/GalacticQ_" target="_blank">GalacticQ <i class="fab fa-twitter"></i></a></li>
+            </ul>
+        </div>
+        <div class="collumn">
+            <h2>Neptune
+                <img src="../img/neptune_emoji.png">
+            </h2>
+            <ul>
+                <li class="cap">Majora</li>
+                <li><a href="https://twitter.com/GledhillWill14" target="_blank">Will <i class="fab fa-twitter"></i></a></li>
+                <li>mirsa</li>
+                <li>Ju4life</li>
+                <li>Zillion</li>
+            </ul>
+        </div>
+        <div class="collumn">
+            <h2>Pluto
+                <img src="../img/pluto_emoji.png">
+            </h2>
+            <ul>
+                <li class="cap"><a href="https://twitter.com/BlitzLeague_" target="_blank">BlitzLeague <i class="fab fa-twitter"></i></a></li>
+                <li><a href="https://twitter.com/Jariz_RL" target="_blank">Jariz <i class="fab fa-twitter" target="_blank"></i></a></li>
+                <li><a href="https://twitter.com/Kevster098" target="_blank">Kevster098 <i class="fab fa-twitter"></i></a></li>
+                <li class="sub"><a href="https://twitter.com/KageruRL" target="_blank">Kageru <i class="fab fa-twitter"></i></a></li>
+            </ul>
         </div>
     </div>
 
